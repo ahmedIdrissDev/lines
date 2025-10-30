401b8b7cb3980a6d6bceef73331e0471642ac8d4
AhmedIdrissDev <webocamw224@gmail.com>
2025-10-30 13:48:51 +0000

    FIX AND PUSH

--- components/ai.tsx
+++ components/ai.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const AI = () => {
   return (
-    <div className='border bg-white p-3 w-full h-20 rounded-2xl flex justify-between items-center border-neutral-200'>
+    <div className='border bg-white bg-linear-60 from-tgcc-400/10 from-20% to-white p-3 w-full h-20 rounded-2xl flex justify-between items-center border-neutral-200'>
        <div className="">
-          <h1>Tgcc AI</h1>
-          <p>Speed up your analysis with TGCC AI.</p>
+          <h1 className='text-2xl '>Tgcc AI</h1>
+          <p className='text-neutral-700'>Speed up your analysis with TGCC AI.</p>
        </div>
        <button className='w-40 h-11 bg-tgcc-500 text-white rounded-2xl'>Get started</button>
     </div>
