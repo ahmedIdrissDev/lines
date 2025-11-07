5176ac4d9a1f906e74521deb7fabaf0b849f577d
AhmedIdrissDev <webocamw224@gmail.com>
2025-11-07 15:23:28 +0100

    fix and push

--- components/kits/customchart.tsx
+++ components/kits/customchart.tsx
@@ -1,19 +1,19 @@
 import { Employee } from '@/types'
-import { ArrowDown, ArrowUp, TrendingDown, TrendingUp } from 'lucide-react'
+import { ArrowDown, ArrowUp, ChartNoAxesColumn, ChartSpline, TrendingDown, TrendingUp, User2 } from 'lucide-react'
 import { object, span } from 'motion/react-client'
 import React from 'react'
 interface  customchartProps{
   lot : string,
   count: number,
-  present: number ,
-  Absent  :number ,
-  fun: Employee[]
+  present: Employee[] ,
+  Absent  :Employee[] ,
 }
-const Customchart = ({Absent ,count ,lot , present ,fun}:customchartProps) => {
-     const  func = Object.groupBy(fun , ({function:fun})=> fun )
+const Customchart = ({Absent ,count ,lot , present }:customchartProps) => {
+     const  func = Object.groupBy(present , ({function:fun})=> fun )
   const data = Object.entries(func).map(([func ,data]) => ({
   func,
   count: data?.filter(({status})=> status==='active' ).length,
+   
   
 }));
   return (
@@ -21,22 +21,27 @@ const Customchart = ({Absent ,count ,lot , present ,fun}:customchartProps) => {
       <div className="w-full rounded-md flex items-center px-2 bg-tgcc-300 h-10">
          <h1 className='font-semibold'>{lot ? lot : 'Project'} </h1>
       </div>
-         <p>{count} </p>
+      <div className="w-full items-center h-30 border border-neutral-200 rounded-2xl flex justify-center">
+         <ChartNoAxesColumn/>
+         <p  className='text-2xl'>{count} </p>
+
+      </div>
          <div className="w-full grid grid-cols-2 gap-2 h-10">
             <div className=" rounded-md flex justify-center items-center w-full bg-tgcc-200/50 text-tgcc-700 gap-2 h-full">
                 <TrendingUp/>
-                {present}
+                {present.length}
             </div>
              <div className=" rounded-md  gap-2 flex justify-center border border-neutral-200 items-center w-full text-tgcc-950 h-full">
                  <TrendingDown/>
 
-                {Absent} Absent
+                {Absent.length} Absent
             </div>
          </div>
          <div className="w-full h-full p-2 rounded-2xl border border-neutral-200">
-            {data.map(({count ,func} , index)=>(
-          <div key={index} className="">{count} {func} </div>
+            {data.map(({count ,func } , index)=>(
+          <div key={index} className="">  {count}  {func} </div>
             ))}
+            
 
          </div>
     </div>
--- components/super.tsx
+++ components/super.tsx
@@ -10,16 +10,15 @@ const Super = () => {
     const mapped = Object.entries(newdata).map(([lot, data]) => ({
   lot,
   count: data?.length,
-  present: data?.filter( ({status}) => status==='active').length ,
-  Absent: data?.filter( ({status}) => status==='inactive').length,
-  fun: data?.filter( ({status}) => status==='active')
+  present: data?.filter( ({status}) => status==='active') ,
+  Absent: data?.filter( ({status}) => status==='inactive'),
 }));
     console.log(newdata)
   return (
     <>
 
     <div className='w-full gap-2.5   grid  grid-cols-3 '>
-        {mapped.splice(0 , 3).map((data , index)=>(
+        {mapped.splice(0 , 8).map((data , index)=>(
           <Customchart key={index} {...data} />
         ) )}
     </div>
