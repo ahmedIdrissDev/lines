a0338fbfb08302569a6b5d7e0a45ccbcb8599f69
AhmedIdrissDev <webocamw224@gmail.com>
2025-11-09 23:19:06 +0100

    fix and push

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -6,14 +6,11 @@ import { Total } from './charts/chart-radial-stacked'
 
 const Chat = () => {
   return (
-    <div className='grid  grid-cols-1 md:grid-cols-4 gap-2 h-max'>
+    <div className='grid  grid-cols-1 md:grid-cols-3 gap-2 h-max'>
          <Total/>
         <ChartTootal/>
         <Other/>
-        <div className="w-full h-full p-4  border flex flex-col justify-center relative items-center border-neutral-200 bg-white rounded-2xl">
-        
-            <button className='w-full h-12 bg-black rounded-xl'>tak a look</button>
-        </div>
+      
     </div>
   )
 }
