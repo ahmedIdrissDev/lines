7fd50a32b5ca56227069b77dadcd68b8350097b7
AhmedIdrissDev <webocamw224@gmail.com>
2025-11-09 21:45:56 +0100

    fix middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,14 @@
 import { withAuth } from "next-auth/middleware"
 
-export default withAuth({
-  pages: {
-    signIn: "/",
-    error: "/error",
+export default withAuth(
+  function middleware(req) {
+    console.log(req.nextauth.token)
   },
-})
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token, 
+       },
+  },
+)
 
+export const config = { matcher: ["/dashboard"] }
