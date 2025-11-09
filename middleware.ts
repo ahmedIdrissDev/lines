b1e61871cc1363cc4c8616d09f3a0358c79cf651
AhmedIdrissDev <webocamw224@gmail.com>
2025-11-09 21:34:15 +0100

    fix and push

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  pages: {
+    signIn: "/",
+    error: "/error",
+  },
+})
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+}
