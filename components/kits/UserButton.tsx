569580247ddd08e6379ac9869caf1b71eb16bfcb
AhmedIdrissDev <webocamw224@gmail.com>
2025-11-09 22:04:43 +0100

    fix and push

--- components/kits/UserButton.tsx
+++ components/kits/UserButton.tsx
@@ -21,7 +21,7 @@ const UserButton = () => {
             exit={{ scale: 0.7, opacity: 0 }} className="bg-white flex justify-between h-1/2  flex-col gap-1.5  p-4 w-1/2  rounded-md border border-neutral-200">
               <div className="">
 
-              <h1>Your account setting</h1>
+              <h1>Paramètres du compte</h1>
                    <div className="flex items-center gap-2">
                   <img  src={data?.user?.image as string} className='w-9 h-9 rounded-full cursor-pointer' />
                    <h1>{data?.user?.name} </h1>
@@ -31,7 +31,7 @@ const UserButton = () => {
                    </div>
               </div>
                    <div className="flex w-full justify-end">
-                       <button onClick={openclose} className='w-20 h-11 bg-tgcc-600 text-white rounded-md'>cancale</button>
+                       <button onClick={openclose} className='w-20 h-11 bg-tgcc-600 text-white rounded-md'>Annuler</button>
                    </div>
             </motion.div>
           </div>
