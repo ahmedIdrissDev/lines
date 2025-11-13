341092581e19e728593e94b12cdfa1a09dfb4485
AhmedIdrissDev <webocamw224@gmail.com>
2025-11-13 23:30:51 +0100

    fix and psuh

--- components/ui/loading.tsx
+++ components/ui/loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='w-60 h-30 rounded-2xl flex justify-center items-center'>
+    <div className='w-60 h-12 rounded-2xl flex  gap-1.5 items-center'>
         <div className="w-5 h-5 bg-black rounded-full animate-caret-blink"/>
         <span>waiting ...</span>
     </div>
