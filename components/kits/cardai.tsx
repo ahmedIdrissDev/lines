48ff0ec409cd6ede831b99bbb1b7a8034560ab6e
AhmedIdrissDev <webocamw224@gmail.com>
2025-12-13 09:34:38 +0100

    fix and  push

--- components/kits/cardai.tsx
+++ components/kits/cardai.tsx
@@ -4,7 +4,7 @@ import { fa } from 'zod/v4/locales'
 
 const CardAI = () => {
   return (
-    <div className='w-full flex flex-col gap-3 justify-center h-70 bg-tgcc-50 rounded-2xl'>
+    <div className='w-full flex p-2 flex-col gap-3 justify-center h-70 bg-tgcc-50 rounded-2xl'>
         <div className="text-center">
         <h1>Agent TGCC</h1>
         <p className='text-sm'>Assistant alimenté par l’IA, connecté à vos données.</p>
--- components/project.tsx
+++ components/project.tsx
@@ -13,6 +13,7 @@ import { getToday, handlePresentsUpdate } from "@/utils";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { toast } from "sonner";
 
 interface Proejct {
   _id: string;
@@ -21,29 +22,29 @@ interface Proejct {
 const Project = () => {
   const { data: users } = useSession();
   const { setdata  ,PojectID ,setProjectId } = store();
+  const [projectdd , setPoject] = useState('')
   const project = users?.user?.project as Proejct[];
-  const ProjectId = PojectID as Id<"Project">
+  const ProjectId = projectdd as Id<"Project">
  /// get project data 
-      const getEmployees =  useQuery(api.functions.employees.employees , ProjectId? {Project:ProjectId} :"skip")
-      const getPresents =  useQuery(api.functions.present.Presents , ProjectId? {Project:ProjectId} :"skip")
-
-
-  const onUser = useEffectEvent(() => {
+ const getPresents =  useQuery(api.functions.present.Presents , projectdd ? {Project:ProjectId} :"skip")
+ const getEmployees =  useQuery(api.functions.employees.employees , projectdd ? {Project:ProjectId} :"skip")
+  
+const onUser = useEffectEvent(() => {
     try {
       if (project[0]._id) {
-        setProjectId(project[0]._id);
+        setProjectId(project[0]._id)
       }
     } catch (error) {
       console.log("error");
     }
   });
-
   const ondata = useEffectEvent(async () => {
     try {
-
+      
+      console.log( "iis",PojectID)
         const today = getToday()
-        const curredlastdate =getPresents?.reverse().find((item) => item)
-        console.log("current last",curredlastdate)
+        const curredlastdate =getPresents?.reverse().find((item) => item) || []
+        console.log("current last",PojectID)
          const Matricule = getPresents?.find(({ date }) => date === today) || curredlastdate ;
                 const Updated = handlePresentsUpdate({
                 Matricule: Matricule?.employees,
@@ -60,13 +61,17 @@ const Project = () => {
   }, [users]);
 
   useEffect(() => {
+
     ondata();
+
   }, [getEmployees , getPresents , PojectID]);
 
   return (
     <>
    
-    <Select onValueChange={(e) => setProjectId(e)}>
+    <Select onValueChange={(e) =>{
+      // setProjectId(e)
+setPoject(e)    } }>
       <SelectTrigger
       
         className="w-12 md:w-[180px] bg-white"
