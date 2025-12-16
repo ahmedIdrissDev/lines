ac066602655b65ebabdd44df8bf5529c386a4a9a
AhmedIdrissDev <webocamw224@gmail.com>
2025-12-16 14:39:55 +0100

    fix and push

--- app/(dashboard)/taches/page.tsx
+++ app/(dashboard)/taches/page.tsx
@@ -3,13 +3,13 @@ import Alert from '@/components/kits/alert'
 import Mail from '@/components/kits/mail'
 import { api } from '@/convex/_generated/api'
 import { useQuery } from 'convex/react'
-import React from 'react'
 
 const page = () => {
   const data = useQuery(api.functions.login.getUsers)
   console.log(data)
   return (
        <>
+       <div className="w-full h-"></div>
        <Alert/>
 
        </>
