9b933ee43c1afa7a56e7ec0e1c671dc6693107a0
AhmedIdrissDev <webocamw224@gmail.com>
2025-12-24 16:51:14 +0100

    fix and push

--- convex/functions/reception.ts
+++ convex/functions/reception.ts
@@ -4,39 +4,41 @@ import { mutation, query } from "../_generated/server";
 export const reception = query({
   args: {
     email: v.string(),
-    userId:v.id("users")
+    userId: v.id("users"),
   },
-  handler: async (ctx, {userId ,email}) => {
-    const messages = await ctx.db.query("emails").order('desc').collect();
+  handler: async (ctx, { userId, email }) => {
+    const messages = await ctx.db.query("emails").order("desc").collect();
     const myemails = messages.filter(({ receptionId }) =>
       receptionId.includes(email)
-  );
-  const  markreadsMessage = Promise.all(myemails.map( async (data , index)=>{
-    const anther = await ctx.db.get(data.userId)
-const seen = await ctx.db
-  .query("seens")
-  .filter(q =>
-    q.and(
-      q.eq(q.field("userId"), userId),
-      q.eq(q.field("messageId"), data._id)
-    )
-  )
-  .first();
-       if(seen) return {
-         ...data ,
-         seens:true ,
-         anther
-       }
-       return {
-        ...data ,
-        seens:false ,
-        anther
-       }
-     }))
+    );
+    const markreadsMessage = Promise.all(
+      myemails.map(async (data, index) => {
+        const anther = await ctx.db.get(data.userId);
+        const seen = await ctx.db
+          .query("seens")
+          .filter((q) =>
+            q.and(
+              q.eq(q.field("userId"), userId),
+              q.eq(q.field("messageId"), data._id)
+            )
+          )
+          .first();
+        if (seen)
+          return {
+            ...data,
+            seens: true,
+            anther,
+          };
+        return {
+          ...data,
+          seens: false,
+          anther,
+        };
+      })
+    );
     return markreadsMessage;
   },
 });
-
 
 export const createReception = mutation({
   args: {
@@ -44,45 +46,84 @@ export const createReception = mutation({
     subject: v.string(),
     body: v.string(),
     file: v.optional(v.array(v.string())),
-    userId:v.id("users") ,
-
+    userId: v.id("users"),
   },
   handler: async (ctx, args) => {
     await ctx.db.insert("emails", args);
   },
 });
 
-
 export const getRespoition = query({
-  args:{
-    id:v.id("emails")
-  } ,
-handler:async (ctx , args)=>{
-     const message = await ctx.db.get(args.id)
-      if(!message) return []
-      const anther =  await ctx.db.get(message.userId)
-      return {
-        message ,
-        anther
-      }
-}
-})
-
+  args: {
+    id: v.id("emails"),
+  },
+  handler: async (ctx, args) => {
+    const message = await ctx.db.get(args.id);
+    if (!message) return [];
+    const anther = await ctx.db.get(message.userId);
+    return {
+      message,
+      anther,
+    };
+  },
+});
 
 /// handleseens
-export const markreadsMessage =mutation({
-  args:{
-       messageId:v.id("emails") ,
-       userId: v.id("users")
-  } ,
-  handler: async (ctx , args)=>{
-     const getMarksMessage = await ctx.db.query("seens").filter(q=> q.and(
-          q.eq(q.field('messageId'), args.messageId) ,
-          q.eq(q.field('userId'), args.userId)
-     ) ).first()
+export const markreadsMessage = mutation({
+  args: {
+    messageId: v.id("emails"),
+    userId: v.id("users"),
+  },
+  handler: async (ctx, args) => {
+    const getMarksMessage = await ctx.db
+      .query("seens")
+      .filter((q) =>
+        q.and(
+          q.eq(q.field("messageId"), args.messageId),
+          q.eq(q.field("userId"), args.userId)
+        )
+      )
+      .first();
 
-     if(!getMarksMessage){
-         await ctx.db.insert("seens",args)
-     }
+    if (!getMarksMessage) {
+      await ctx.db.insert("seens", args);
+    }
+  },
+});
+
+// add reply functions 
+export const  getMessagesReply = query({
+  args:{
+    messageId:v.id("emails")
+  } ,
+  handler:async (ctx , args)=>{
+         try {
+          if(!args.messageId) return []
+          const replys = await ctx.db.query('reply').filter(q=> q.eq(q.field('messageId'), args.messageId)).collect()
+          return Promise.all(replys.map(async( data , index)=>{
+            const anther = await ctx.db.get(data.antherId)
+            return {
+              ...data ,
+              anther
+            }
+          }))
+         } catch (error) {
+          
+         }
+  }
+})
+export const  PostMessagesReply = mutation({
+  args:{
+    messageId: v.id("emails"),
+    body: v.string(),
+    antherId: v.id("users"),    
+  } ,
+  handler:async (ctx , args)=>{
+         try {
+          if(!args.messageId || args.body || args.antherId) return []
+           await ctx.db.insert('reply' ,args)
+         } catch (error) {
+            console.log(error)
+         }
   }
 })
--- convex/schema.ts
+++ convex/schema.ts
@@ -2,49 +2,56 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  
   users: defineTable({
     email: v.string(),
     func: v.string(),
     name: v.string(),
     role: v.string(),
-    password:v.string(),
-    image:v.optional(v.string()) ,
-    project:v.array(v.id('Project')) ,
+    password: v.string(),
+    image: v.optional(v.string()),
+    project: v.array(v.id("Project")),
   }).index("project_id", ["project"]),
-  
-   employees: defineTable({
+
+  employees: defineTable({
     Matricule: v.number(),
     firstname: v.optional(v.string()),
     lastname: v.optional(v.string()),
-    function:v.optional(v.string()),
-    siteManger: v.string() ,
+    function: v.optional(v.string()),
+    siteManger: v.string(),
     status: v.union(v.literal("active"), v.literal("inactive")),
-    Project:v.id("Project"),
+    Project: v.id("Project"),
     createdAt: v.string(),
   }).index("project_id", ["Project"]),
 
-
   Present: defineTable({
     date: v.string(),
-    employees: v.array(v.number() ) ,
-    Project:v.optional(v.id("Project")),
-  }).index("project_id", ["Project"]) ,
+    employees: v.array(v.number()),
+    Project: v.optional(v.id("Project")),
+  }).index("project_id", ["Project"]),
 
   Project: defineTable({
-    name:v.string() ,
-    type:v.optional(v.string())
-  }) ,
+    name: v.string(),
+    type: v.optional(v.string()),
+  }),
   emails: defineTable({
-    userId:v.id("users") ,
-    receptionId: v.array(v.string()) ,
-    subject:v.string(),
-    body:v.string() ,
-    file:v.optional(v.array(v.string()))
-  }) ,
+    userId: v.id("users"),
+    receptionId: v.array(v.string()),
+    subject: v.string(),
+    body: v.string(),
+    file: v.optional(v.array(v.string())),
+  }),
 
   seens: defineTable({
-       messageId:v.id("emails") ,
-       userId:v.id("users")
-  }).index("message_id", ["messageId"]).index("user_id", ["userId"])
+    messageId: v.id("emails"),
+    userId: v.id("users"),
+  })
+    .index("message_id", ["messageId"])
+    .index("user_id", ["userId"]),
+  reply: defineTable({
+    messageId: v.id("emails"),
+    body: v.string(),
+    antherId: v.id("users"),
+  })
+    .index("message_id", ["messageId"])
+    .index("user_id", ["antherId"]),
 });
